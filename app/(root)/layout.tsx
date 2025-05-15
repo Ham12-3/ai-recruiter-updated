69654818730e20e1b2aa8473eeacd58c8598fbad
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-05-15 20:50:38 +0100

    update

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,25 +1,7 @@
-import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
-import "./globals.css";
+import React, { ReactNode } from "react";
 
-const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Prepwise",
-  description: "An AI-powered platform for preparing for mock inteviews",
+const RootLayout = ({ children }: { children: ReactNode }) => {
+  return <div>{children}</div>;
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en" className="dark">
-      <body className={`${monaSans.className} antialiased`}>{children}</body>
-    </html>
-  );
-}
+export default RootLayout;
